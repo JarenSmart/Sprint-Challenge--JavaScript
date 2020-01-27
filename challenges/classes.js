d15c85f2c498ad65c684b27953c9d1b469db44bb
jaren <jarensmart@yahoo.com>
2020-01-26 22:12:19 -0500

    finished objects-array.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -41,7 +41,7 @@ class CuboidMaker2 {
 const cuboid2 = new CuboidMaker2(4, 5, 5);
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid2.volume()); // 100
-console.log(cuboid2.surfaceArea()); // 130
+//console.log(cuboid2.volume()); // 100
+//console.log(cuboid2.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/functions.js
+++ challenges/functions.js
@@ -28,9 +28,9 @@ function greeting(firstname, lastname) {
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-console.log(consume(2, 2, add)); // 4
-console.log(consume(10, 16, multiply)); // 160
-console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+//console.log(consume(2, 2, add)); // 4
+//console.log(consume(10, 16, multiply)); // 160
+//console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ====
 
@@ -42,11 +42,11 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 const external = "I'm outside the function";
 
 function myFunction() {
-  console.log(external);
+  //console.log(external);
   const internal = "Hello! I'm inside myFunction!";
 
   function nestedFunction() {
-    console.log(internal);
+    //console.log(internal);
   }
   nestedFunction();
 }
